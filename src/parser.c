8d1bd5bf7e116239d7250dcfa49fde1d0cb997dd
RunThem <iccy.fun@outlook.com>
2023-06-09 17:35:44 +0800

    添加语法解析错误处理

--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,39 @@
 #include "parser.h"
+#include "token.h"
+
+#undef dis
+#define dis(fmt, ...) fprintf(stderr, fmt __VA_OPT__(, ) __VA_ARGS__)
+
+typedef struct {
+  token_t cur;
+  token_t pre;
+  bool has_err; /* 如果有错误, 为真 */
+} parser_t;
+
+parser_t parse = {0};
+
+static void err_at(token_t* tok, c_str msg) {
+  dis("[line %d] error:", tok->line);
+
+  if (tok->kind == T_EOF) {
+    dis(" at end");
+  } else if (tok->kind == T_ERROR) {
+    /* TODO: nothing */
+  } else {
+    dis(" at '%s'", tok->tok->c_str);
+  }
+
+  dis(": %s\n", msg);
+  parse.has_err = true;
+}
+
+static void err_at_cur(c_str msg) {
+  err_at(&parse.cur, msg);
+}
+
+static void err_at_pre(c_str msg) {
+  err_at(&parse.pre, msg);
+}
 
 void parser(tokens_t* tokens) {
   list_for(&tokens->toks, it) {
